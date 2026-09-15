df89cf4b96d8e4534c9793a6040e6cd112ba2d9f
krishna-gupta <kg0093287@gmail.com>
2026-09-15 10:24:13 +0530

    create a dashboard for admin and employess

--- src/main/java/com/krishna/ems/repository/TaskRepository.java
+++ src/main/java/com/krishna/ems/repository/TaskRepository.java
@@ -1,6 +1,7 @@
 package com.krishna.ems.repository;
 
 import com.krishna.ems.entity.Task;
+import com.krishna.ems.entity.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +11,13 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByProjectId(Long projectId);
 
     List<Task> findByAssignedToId(Long employeeId);
+
+    long countByAssignedToId(Long employeeId);
+
+    long countByAssignedToIdAndStatus(
+            Long employeeId,
+            TaskStatus status
+    );
+
+    long countByStatus(TaskStatus status);
 }
